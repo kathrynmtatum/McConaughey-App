abb6cb7a61df2782aaeff07b8fd9a3d4e61e2fc8
Kathryn Tatum <tatumka@bc.edu>
2020-06-09 10:56:45 -0400

    Final Push

--- McConaughey/ViewController.swift
+++ McConaughey/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var matthewImage: UIImageView!
+    @IBOutlet weak var matthewQuestion: UILabel!
+    @IBOutlet weak var alrightLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        alrightLabel.text = ""
     }
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        alrightLabel.text = "\(Int(stepper.value))"
+    }
+    
 
-
+    func playSound(name: String, numberOfLoops: Int) {
+           if let sound = NSDataAsset(name: name) {
+               do {
+                   try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.numberOfLoops = numberOfLoops
+                   audioPlayer.play()
+               } catch {
+                   print ("ERROR: 🛑 \(error.localizedDescription) Could not initialize AVAudioPlayer object")
+               }
+           } else {
+               print("ERROR: 🛑 Could not read data from file sound0")
+           }
+    
 }
-
+    
+    @IBAction func messageButtonPressed(_ sender: Any) {
+        playSound(name: "alright", numberOfLoops: Int(stepper.value)-1)
+    }
+    
+}
